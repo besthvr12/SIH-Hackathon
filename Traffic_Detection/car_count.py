1032f49514f4cf7f1f7859d7c436ee553b046822
Garima Gupta <33591125+Garimagupta85@users.noreply.github.com>
2020-02-08 08:33:34 +0530

    Update car_count.py

--- Traffic_Detection/car_count.py
+++ Traffic_Detection/car_count.py
@@ -12,18 +12,26 @@ import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
-starttimevar = time.time()
-im = cv2.imread('carjam.jpeg')
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
-print('Number of cars in the image is '+ str(label.count('car')))
-elapsetime = time.time() - starttimevar
-print(elapsetime)
 
 import os 
 import re
+
+vidcap = cv2.VideoCapture('/content/Traffic in Bangalore is now a nuisance.mp4')
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+      cv2.imwrite("/content/ Frames/"+str(count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.5 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
 
 def atof(text):
     try:
@@ -45,20 +53,19 @@ import os
 folder_path = "/content/ Frames/"
 starttimevar = time.time()
 l = []
-
-# for data_file in sorted(os.listdir(folder_path), key= natural_keys):
-  # im = cv2.imread(folder_path+'/'+data_file)
-im = cv2.imread("/content/ Frames/76.jpg")
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
-print('Number of cars in the ' +data_file +' is '+ str(label.count('car')))
-print('Number of cars in the ' +data_file +' is '+ str(label.count('bus')))
-print('Number of cars in the ' +data_file +' is '+ str(label.count('bicycle')))
-print('Number of cars in the ' +data_file +' is '+ str(label.count('motorbike')))
-print('Number of cars in the ' +data_file +' is '+ str(label.count('truck')))
-  # l.append(str(label.count('car')))
+for data_file in sorted(os.listdir(folder_path), key= natural_keys):
+    im = cv2.imread(folder_path+'/'+data_file)
+    #im = cv2.imread("/content/ Frames/76.jpg")
+    bbox, label, conf = cv.detect_common_objects(im)
+    output_image = draw_bbox(im, bbox, label, conf)
+    plt.imshow(output_image)
+    plt.show()
+    print('Number of cars in the ' +data_file +' is '+ str(label.count('car')))
+    print('Number of cars in the ' +data_file +' is '+ str(label.count('bus')))
+    print('Number of cars in the ' +data_file +' is '+ str(label.count('bicycle')))
+    print('Number of cars in the ' +data_file +' is '+ str(label.count('motorbike')))
+    print('Number of cars in the ' +data_file +' is '+ str(label.count('truck')))
+    l.append(str(label.count('car')))
 elapsetime = time.time() - starttimevar
 print(elapsetime)
 print(l)
